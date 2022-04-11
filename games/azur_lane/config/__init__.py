0bfa9b02b122a41d417de8045626d3f017e6783f
ShaoyenYu <shaoyenyu@gmail.com>
2022-04-12 03:18:34 +0800

    [REF] refactor project structure ---
    + move UI definition files to `interface`;
    + remove test code in module `config`;

--- games/azur_lane/config/__init__.py
+++ games/azur_lane/config/__init__.py
@@ -1,21 +1,10 @@
 from pathlib import Path
 
-from matplotlib import pyplot as plt
-
 CONFIG_DIR = Path(__file__).parent
 
 DIR_BASE = CONFIG_DIR.parent.as_posix()
-CONFIG_SCENE = f"{DIR_BASE}/config/scene.yaml"
-CONFIG_DELEGATION = f"{DIR_BASE}/config/delegation.yaml"
+DIR_INTERFACE = f"{DIR_BASE}/interface"
 DIR_TESTCASE = f"{DIR_BASE}/assets/testcase"
 
-
-def close_figure(event):
-    if event.key == 'escape':
-        plt.close(event.canvas.figure)
-
-
-def s(img, title=""):
-    plt.imshow(img)
-    plt.title(title)
-    plt.show()
+CONFIG_SCENE = f"{DIR_INTERFACE}/scene.yaml"
+CONFIG_DELEGATION = f"{DIR_INTERFACE}/delegation.yaml"

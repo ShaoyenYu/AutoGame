80142c4d961c76f7c4a9103314a4194d83014b33
ShaoyenYu <shaoyenyu@gmail.com>
2023-01-30 14:03:18 +0800

    [BLD] provide function `launch` in module `games.azur_lane`

---
+++ games/__init__.py
@@ -0,0 +1,1 @@
+from .azur_lane.launch_game import launch as launch_azurlane
--- games/azur_lane/launch_game.py
+++ games/azur_lane/launch_game.py
@@ -1,7 +1,9 @@
 from script import boot
 
-if __name__ == '__main__':
+
+def launch():
     window_name = "BS_AzurLane"
     launch_cmd = "HD-Player.exe --instance Rvc64 --cmd launchApp --package \"com.hkmanjuu.azurlane.gp\""
 
-    boot.launch(window_name, launch_cmd)
+    simulator = boot.launch(window_name, launch_cmd)
+    return simulator
